243687ef956619efb225218e9c5e982556bb0eed
Tran Hai Bac <Fariak@tinhs-MBP.beeline>
2022-02-12 23:13:06 +0300

    update

--- packages/solana_dart/lib/src/solana_api.dart
+++ packages/solana_dart/lib/src/solana_api.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart' as network;
 import 'package:solana_dart/src/request.dart';
 import 'package:solana_dart/src/response.dart';
-import 'package:solana_dart/src/results/vote_accounts_data.dart';
+import 'package:solana_dart/src/results/perf_samples.dart';
+import 'package:solana_dart/src/results/vote_accounts.dart';
 
 class SolanaApi {
   final network.Dio dio;
@@ -14,12 +15,15 @@ class SolanaApi {
     return Response.fromMap(response.data);
   }
 
-  Future<Response<VoteAccountsData>> getVoteAccount() async {
+  Future<Response<VoteAccounts>> getVoteAccount() async {
     final request = Request.v2(method: "getVoteAccounts");
     var response = await dio.post('', data: request.toMap());
-    return Response.fromMapWithDecoder(response.data, VoteAccountsData.fromMap);
+    return Response.fromMapWithDecoder(response.data, VoteAccounts.fromMap);
   }
 
-  Future<Response<>> getRecentPerformanceSamples() async {
+  Future<Response<List<PerfSamples>>> getRecentPerformanceSamples() async {
+    final request = Request.v2(method: "getRecentPerformanceSamples");
+    var response = await dio.post('', data: request.toMap());
+    return Response.fromMapWithDecoder(response.data, PerfSamples.fromArray);
   }
 }
--- packages/solana_dart/test/solana_dart_test.dart
+++ packages/solana_dart/test/solana_dart_test.dart
@@ -1,12 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:solana_dart/solana_dart.dart';
+import 'package:solana_dart/src/results/perf_samples.dart';
+import 'package:solana_dart/src/results/vote_accounts.dart';
+import 'package:solana_dart/src/solana_api.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('getTransactionCount', () async {
+    SolanaApi api = SolanaApi('https://api.devnet.solana.com');
+    final response = await api.getTransactionCount();
+    expect(response.result, isInstanceOf<int>());
+  });
+
+  test('getPerfSamples', () async {
+    SolanaApi api = SolanaApi('https://api.devnet.solana.com');
+    final response = await api.getRecentPerformanceSamples();
+    expect(response.result, isInstanceOf<List<PerfSamples>>());
+  });
+
+  test('getVoteAccount', () async {
+    SolanaApi api = SolanaApi('https://api.devnet.solana.com');
+    final response = await api.getVoteAccount();
+    expect(response.result, isInstanceOf<VoteAccounts>());
   });
 }
