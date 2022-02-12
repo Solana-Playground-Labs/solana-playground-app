f666e1b8de5841ad938f0943f9db4f6bd1965ae1
Tran Hai Bac <Fariak@tinhs-MBP.beeline>
2022-02-12 15:38:06 +0300

    feature(homepage):primary_design

---
+++ lib/repository/solana_repository.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+
+class SolanaRepository {
+  final String endpoint;
+  final Dio dio;
+  SolanaRepository(this.endpoint) : dio = Dio(BaseOptions(baseUrl: endpoint));
+
+  Future<int> getTransactionCount() async {
+    var response = await dio.post('',
+        data: {"jsonrpc": "2.0", "id": 3, "method": "getTransactionCount"});
+
+    return response.data['result'];
+  }
+
+  Future<int> getValidatorNodesCount() async {
+    var response = await dio.post('',
+        data: {"jsonrpc": "2.0", "id": 2, "method": "getVoteAccounts"});
+
+    return response.data['result']['current'].length;
+  }
+
+  Future<double> getTransactionPerSecond() async {
+    var response = await dio.post('', data: {
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": "getRecentPerformanceSamples",
+      "params": [10]
+    });
+
+    var sum = 0.0;
+    for (var i = 0; i < response.data['result'].length; i++) {
+      final sample = response.data['result'][i];
+      final double tps = sample['numTransactions'] / sample['samplePeriodSecs'];
+      sum = sum + tps;
+    }
+    return sum / response.data['result'].length;
+  }
+}
--- lib/scene/home/view/home_view.dart
+++ lib/scene/home/view/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:solana_playground_app/route/app_router.gr.dart';
+import 'package:solana_playground_app/scene/home/widget/solana_card.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:solana_playground_app/scene/home/widget/solana_cookbook-card.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -8,14 +11,35 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Editor"),
-          onPressed: () {
-            context.router.navigate(const EditorRoute());
-          },
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('SOLANA', style: TextStyle(color: Colors.white)),
+        centerTitle: false,
+        actions: [
+          SvgPicture.asset('assets/icons/user.svg'),
+          Center(
+              child: Padding(
+            padding: EdgeInsets.only(left: 10, right: 20),
+            child: Text("@darklong97x"),
+          )),
+        ],
+      ),
+      body: Column(
+        children: [
+          Wrap(
+            spacing: 35,
+            children: [
+              SolanaCard(),
+              SolanaCookBookCard(),
+            ],
+          ),
+          ElevatedButton(
+            child: const Text("Editor"),
+            onPressed: () {
+              context.router.navigate(const EditorRoute());
+            },
+          ),
+        ],
       ),
     );
   }
